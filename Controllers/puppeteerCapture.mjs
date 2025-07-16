52d9ac26d644e7332db316bb8bcd7e63c164d183
Abdelrahman Galal <abdelrahman.m.galal@gmail.com>
2025-07-16 13:47:37 +0300

    remove executablePath

--- Controllers/puppeteerCapture.mjs
+++ Controllers/puppeteerCapture.mjs
@@ -15,14 +15,13 @@ export async function captureMapWithLeaflet({
   const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: { width, height },
-    executablePath: '/usr/bin/chromium-browser',
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox'
     ]
   });
   const page = await browser.newPage();
-
+  
   const tileUrl = tileUrlTemplate ||
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
