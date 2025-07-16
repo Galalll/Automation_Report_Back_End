4c2fac4e2bae7f95378a86fdfdc2b3349c0312a0
Abdelrahman Galal <abdelrahman.m.galal@gmail.com>
2025-07-16 13:32:21 +0300

    chromium-browser

--- Controllers/puppeteerCapture.mjs
+++ Controllers/puppeteerCapture.mjs
@@ -14,7 +14,12 @@ export async function captureMapWithLeaflet({
 }) {
   const browser = await puppeteer.launch({
     headless: true,
-    defaultViewport: { width, height }
+    defaultViewport: { width, height },
+    executablePath: '/usr/bin/chromium-browser',
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
   });
   const page = await browser.newPage();
 
